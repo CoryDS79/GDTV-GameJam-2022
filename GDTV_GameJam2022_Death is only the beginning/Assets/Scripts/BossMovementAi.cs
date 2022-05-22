451591b94d805639f9023b19054e57465901d200
CoryDS79 <coryds79@gmail.com>
2022-05-22 07:29:01 -0400

    Boss Weapons
    Boss movement resolved
    WIP on boss weapons

---
+++ GDTV_GameJam2022_Death is only the beginning/Assets/Scripts/BossMovementAi.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovementAi : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] private float timeToMove = 5f;
+    [SerializeField] Vector2 startPos = new Vector2(0, 0);
+    private float moveTimer;
+    Vector2 newRandomPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newRandomPos = startPos;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ChooseRandomPosAtIntervals();
+        MoveTowardsPosition(newRandomPos);
+    }
+
+
+    void ChooseRandomPosAtIntervals()
+    {
+        moveTimer += Time.deltaTime;
+        if (moveTimer >= timeToMove)
+        {
+            RandomizePosition();
+            moveTimer = 0;
+        }
+    }
+
+    private void MoveTowardsPosition(Vector2 target)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    private Vector2 RandomizePosition()
+    {
+        newRandomPos = new Vector2(Random.Range(-3.5f, 3.5f), Random.Range(0f, 4f));
+        return newRandomPos;
+    }
+
+}
+
